9f954ef3547c0799736bb91cb6f822701dd17e78
kayla133 <makaylahansen04@gmail.com>
2026-01-17 17:27:45 -0700

    notes

--- nps_one/src/js/main.js
+++ nps_one/src/js/main.js
@@ -1,3 +1,16 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+function setHeaderInfo(data) {
+    const displaimer = document.querySelector(".disclaimer > a");
+    disclaimer.href = data.url;
+    displaimer.innerHTML = data.fullName;
+
+    document.querySelector("head > title").textContent = data.fullName;
+
+    document.querySelector(".hero-banner > img").src = data.images[0].url;
+
+    document.querySelector(".hero-banner__content").innerHTML = 
+        parkInfoTemplate(data);
+}
